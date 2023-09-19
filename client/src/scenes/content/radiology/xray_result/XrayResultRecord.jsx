3d56d4a5204b691e218661bd5d4cbc4d0cca8566
Mhan1999 <abdulsamadadzmani@gmail.com>
2023-09-19 20:57:00 +0800

    Added New Features

--- client/src/scenes/content/radiology/xray_result/XrayResultRecord.jsx
+++ client/src/scenes/content/radiology/xray_result/XrayResultRecord.jsx
@@ -19,8 +19,7 @@ const XrayResultRecord = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     searchPatients(search, setSearchResult1);
-
-  }
+  };
   return (
     <>
       <GridComponent
@@ -29,15 +28,96 @@ const XrayResultRecord = () => {
         onChange={(e) => setSearch(e.target.value)}
         title='X-ray Result Record'
         icon={<FlipOutlined sx={{ fontSize: 30 }} />}>
-          {searchResult1.length === 0 ? <>
+        {searchResult1.length === 0 ? (
+          <>
             {searchResult &&
-          searchResult.map((index) => (
+              searchResult.map((index) => (
+                <ContentGrid item xs={12} md={3.5} position='relative'>
+                  <Stack
+                    direction='row'
+                    sx={{ position: "absolute", right: 2, bottom: 1 }}>
+                    {index.xray_result.length === 0 ? (
+                      <EmptyModal message='No X-ray result Made Yet' />
+                    ) : (
+                      <XrayResultReadMoreModal index={index} />
+                    )}
+                  </Stack>
+                  <Typography
+                    color='primary'
+                    variant='h4'
+                    sx={{
+                      p: "2px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      border: "2px solid #f4f4f4",
+                      borderRadius: 2,
+                      width: "100px",
+                      maxWidth: "100px",
+                    }}>
+                    {index.formerOPD ? "OPD" : "IPD"}
+                  </Typography>
+
+                  <Stack spacing={0.5}>
+                    <Typography
+                      textTransform='uppercase'
+                      variant='body2'
+                      fontWeight={600}>
+                      {String(index.firstName).length > 15
+                        ? String(index.firstName).substring(0, 15) + "..."
+                        : index.firstName}
+                    </Typography>
+                    <Typography
+                      textTransform='uppercase'
+                      variant='body2'
+                      fontWeight={600}>
+                      {String(index.middleName).length > 15
+                        ? String(index.middleName).substring(0, 15) + "..."
+                        : index.middleName}
+                    </Typography>
+                    <Typography
+                      textTransform='uppercase'
+                      variant='body2'
+                      fontWeight={600}>
+                      {String(index.lastName).length > 15
+                        ? String(index.lastName).substring(0, 15) + "..."
+                        : index.lastName}
+                    </Typography>
+
+                    <Typography
+                      color='primary'
+                      variant='body2'
+                      fontWeight={600}>
+                      {index.formerOPD
+                        ? String(index.createdAt).substring(0, 10)
+                        : index.admission && index.admission.date}
+                    </Typography>
+                    <Typography
+                      color='primary'
+                      variant='body2'
+                      fontWeight={600}>
+                      {index.formerOPD
+                        ? String(index.createdAt).substring(11, 16)
+                        : index.admission && index.admission.time}
+                    </Typography>
+                  </Stack>
+                </ContentGrid>
+              ))}
+          </>
+        ) : (
+          searchResult1.map((index) => (
             <ContentGrid item xs={12} md={3.5} position='relative'>
-              <Stack direction='row' sx={{ position: "absolute", right: 2, bottom: 1 }}>
-                {index.prescription.length === 0 ? <EmptyModal message='No X-ray result Made Yet'/> : <XrayResultReadMoreModal index={index} />}
+              <Stack
+                direction='row'
+                sx={{ position: "absolute", right: 2, bottom: 1 }}>
+                {index.prescription.length === 0 ? (
+                  <EmptyModal message='No X-ray result Made Yet' />
+                ) : (
+                  <XrayResultReadMoreModal index={index} />
+                )}
               </Stack>
               <Typography
-              color='primary'
+                color='primary'
                 variant='h4'
                 sx={{
                   p: "2px",
@@ -52,18 +132,31 @@ const XrayResultRecord = () => {
                 {index.formerOPD ? "OPD" : "IPD"}
               </Typography>
 
-              <Stack spacing={0.5} >
-              <Typography textTransform='uppercase' variant='body2' fontWeight={600}>{String(index.firstName).length > 15
-                        ? String(index.firstName).substring(0, 15) + "..."
-                        : index.firstName}</Typography>
-                    <Typography textTransform='uppercase' variant='body2' fontWeight={600}>{String(index.middleName).length > 15
-                        ? String(index.middleName).substring(0, 15) + "..."
-                        : index.middleName}</Typography>
-                    <Typography textTransform='uppercase' variant='body2' fontWeight={600}>
-                      {String(index.lastName).length > 15
-                        ? String(index.lastName).substring(0, 15) + "..."
-                        : index.lastName}
-                    </Typography>
+              <Stack spacing={0.5}>
+                <Typography
+                  textTransform='uppercase'
+                  variant='body2'
+                  fontWeight={600}>
+                  {String(index.firstName).length > 15
+                    ? String(index.firstName).substring(0, 15) + "..."
+                    : index.firstName}
+                </Typography>
+                <Typography
+                  textTransform='uppercase'
+                  variant='body2'
+                  fontWeight={600}>
+                  {String(index.middleName).length > 15
+                    ? String(index.middleName).substring(0, 15) + "..."
+                    : index.middleName}
+                </Typography>
+                <Typography
+                  textTransform='uppercase'
+                  variant='body2'
+                  fontWeight={600}>
+                  {String(index.lastName).length > 15
+                    ? String(index.lastName).substring(0, 15) + "..."
+                    : index.lastName}
+                </Typography>
 
                 <Typography color='primary' variant='body2' fontWeight={600}>
                   {index.formerOPD
@@ -77,57 +170,8 @@ const XrayResultRecord = () => {
                 </Typography>
               </Stack>
             </ContentGrid>
-           
-          ))} 
-          
-          
-           </> : searchResult1.map((index) => (
-              <ContentGrid item xs={12} md={3.5} position='relative'>
-              <Stack direction='row' sx={{ position: "absolute", right: 2, bottom: 1 }}>
-              {index.prescription.length === 0 ? <EmptyModal message='No X-ray result Made Yet'/> : <XrayResultReadMoreModal index={index} />}
-              </Stack>
-              <Typography
-              color='primary'
-                variant='h4'
-                sx={{
-                  p: "2px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  border: "2px solid #f4f4f4",
-                  borderRadius: 2,
-                  width: "100px",
-                  maxWidth: "100px",
-                }}>
-                {index.formerOPD ? "OPD" : "IPD"}
-              </Typography>
-
-              <Stack spacing={0.5} >
-              <Typography textTransform='uppercase' variant='body2' fontWeight={600}>{String(index.firstName).length > 15
-                        ? String(index.firstName).substring(0, 15) + "..."
-                        : index.firstName}</Typography>
-                    <Typography textTransform='uppercase' variant='body2' fontWeight={600}>{String(index.middleName).length > 15
-                        ? String(index.middleName).substring(0, 15) + "..."
-                        : index.middleName}</Typography>
-                    <Typography textTransform='uppercase' variant='body2' fontWeight={600}>
-                      {String(index.lastName).length > 15
-                        ? String(index.lastName).substring(0, 15) + "..."
-                        : index.lastName}
-                    </Typography>
-
-                <Typography color='primary' variant='body2' fontWeight={600}>
-                  {index.formerOPD
-                    ? String(index.createdAt).substring(0, 10)
-                    : index.admission && index.admission.date}
-                </Typography>
-                <Typography color='primary' variant='body2' fontWeight={600}>
-                  {index.formerOPD
-                    ? String(index.createdAt).substring(11, 16)
-                    : index.admission && index.admission.time}
-                </Typography>
-              </Stack>
-            </ContentGrid> ))}
-        
+          ))
+        )}
       </GridComponent>
     </>
   );
--- client/src/scenes/layout/radiology/xray_result/XrayResultReadMoreModal.jsx
+++ client/src/scenes/layout/radiology/xray_result/XrayResultReadMoreModal.jsx
@@ -29,76 +29,81 @@ const XrayResultReadMoreModal = ({ index }) => {
         <ReadMoreBtn handleOpen={handleOpen} />
         <ModalComponent open={open} mdWidth={600}>
           <BackBtn handleClose={handleClose1} />
-          <Stack spacing={2} sx={{ mt: 8 }}>
-            <Typography variant='h5' sx={{ fontWeight: 600 }} color='primary'>
-              X-ray Result Info
-            </Typography>
+          <Box pb={2} pr={2} pl={2}>
+            <Stack spacing={2} sx={{ mt: 8 }}>
+              <Typography variant='h5' sx={{ fontWeight: 600 }} color='primary'>
+                X-ray Result Info
+              </Typography>
 
-            <Stack spacing={2}>
-              <Stack spacing={2} direction='row' alignItems='center'>
-                <Typography color='primary'>X-ray Price:</Typography>
-                <Typography>{index.xrayPrice}</Typography>
+              <Stack spacing={2}>
+                <Stack spacing={2} direction='row' alignItems='center'>
+                  <Typography color='primary'>X-ray Price:</Typography>
+                  <Typography>{index.xrayPrice}</Typography>
+                </Stack>
+                <TableContainer
+                  component={Box}
+                  sx={{
+                    maxHeight: "400px",
+                    overflowX: { xs: "shown", md: "hidden" },
+                    height: "100%",
+                    "::-webkit-scrollbar ": {
+                      width: "3px",
+                      height: { xs: "3px", md: "0px" },
+                    },
+                    "::-webkit-scrollbar-thumb": {
+                      backgroundColor: "#1565c08f",
+                    },
+                  }}>
+                  <Table stickyHeader aria-label='sticky table'>
+                    <Stack
+                      direction='row'
+                      spacing={22}
+                      sx={{
+                        position: "sticky",
+                        top: 0,
+                        background: "#fff",
+                        mb: 3,
+                        pb: 2,
+                        borderBottom: "1px solid gray",
+                      }}>
+                      <Typography sx={{ fontWeight: 600, fontSize: "17px" }}>
+                        Image
+                      </Typography>
+                      <Typography sx={{ fontWeight: 600, fontSize: "17px" }}>
+                        Remarks
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={2}>
+                      {index.xray_result &&
+                        index.xray_result.map((rad) => (
+                          <>
+                            <Stack direction='row' spacing={3} key={rad._id}>
+                              <img
+                                src={rad.image}
+                                alt='pic'
+                                style={{
+                                  width: "200px",
+                                  height: "200px",
+                                  objectFit: "cover",
+                                  borderRadius: "3px",
+                                }}
+                              />
+                              <Typography
+                                sx={{ width: "200px", maxWidth: "200px" }}>
+                                {rad.details}
+                              </Typography>
+                            </Stack>
+                          </>
+                        ))}
+                    </Stack>
+                  </Table>
+                </TableContainer>
               </Stack>
-              <TableContainer
-                component={Box}
-                sx={{
-                  maxHeight: "400px",
-                  overflowX: { xs: "shown", md: "hidden" },
-                  height: "100%",
-                  "::-webkit-scrollbar ": {
-                    width: "3px",
-                    height: { xs: "3px", md: "0px" },
-                  },
-                  "::-webkit-scrollbar-thumb": {
-                    backgroundColor: "#1565c08f",
-                  },
-                }}>
-                <Table stickyHeader aria-label='sticky table'>
-                  <Stack
-                    direction='row'
-                    spacing={22}
-                    sx={{
-                      position: "sticky",
-                      top: 0,
-                      background: "#fff",
-                      mb: 3,
-                      pb: 2,
-                      borderBottom: "1px solid gray",
-                    }}>
-                    <Typography sx={{ fontWeight: 600, fontSize: "17px" }}>
-                      Image
-                    </Typography>
-                    <Typography sx={{ fontWeight: 600, fontSize: "17px" }}>
-                      Remarks
-                    </Typography>
-                  </Stack>
-                  <Stack spacing={2}>
-                    {index.xray_result &&
-                      index.xray_result.map((rad) => (
-                        <>
-                          <Stack direction='row' spacing={3} key={rad._id}>
-                            <img
-                              src={rad.image}
-                              alt='pic'
-                              style={{
-                                width: "200px",
-                                height: "200px",
-                                objectFit: "cover",
-                                borderRadius: "3px",
-                              }}
-                            />
-                            <Typography>{rad.details}</Typography>
-                          </Stack>
-                        </>
-                      ))}
-                  </Stack>
-                </Table>
-              </TableContainer>
+              <Stack direction='row-reverse'>
+                {/* <XrayResultEditModal index={index} /> */}
+              </Stack>
             </Stack>
-            <Stack direction='row-reverse'>
-              {/* <XrayResultEditModal index={index} /> */}
-            </Stack>
-          </Stack>
+          </Box>
         </ModalComponent>
       </Box>
     </>
